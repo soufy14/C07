bafff2bd4411f552090353bd1aa54dc3eb67978a
YACOUBA ASSOUMANE Soufianou <yacoubaassoumanesoufianou@iMac-de-YACOUBA.local>
2025-07-23 17:35:06 +0100

    c

--- count_words.c
+++ count_words.c
@@ -5,14 +5,14 @@
 int main() {
     char tab[100];
     int count = 0, i = 0;  // initialisation de i
-
+     int in_word = 0;
     fgets(tab, sizeof(tab), stdin);  // lire une phrase avec espaces
     
-    int in_word = 0;
 
     while (tab[i] != '\0') {
-        if (isspace(tab[i]) || ispunct(tab[i])) {
-            in_word = 0;  // Fin d’un mot
+        if (isspace(tab[i]) || (ispunct(tab[i]) && tab[i] != '\'')) {
+    in_word = 0;
+
         } else if (!in_word) {
             in_word = 1;  // Début d’un mot
             count++;
