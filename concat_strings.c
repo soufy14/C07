76f40782ded16158e02957b4a807099aa33d0fb3
YACOUBA ASSOUMANE Soufianou <yacoubaassoumanesoufianou@iMac-de-YACOUBA.local>
2025-07-23 15:46:57 +0100

    c

---
+++ concat_strings.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    char tab1[100], tab2[100];
+    int l2 = 0, l1 = 0;
+scanf("%s", tab1);
+scanf("%s", tab2);
+while (tab1[l1] != '\0')
+{
+    l1++;
+}
+while (tab1[l2] != '\0')
+{
+    l2++;
+}
+    for (int i = 0; i <= l2; i++){
+        tab1[l1 + i] = tab2[i];
+    }
+
+    printf("%s\n", tab1);
+    return 0;
+}
